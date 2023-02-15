7499569e43d3864d87b7290c89e1bde0dbd6f4e6
delarosaii17 <tacaltacc@gmail.com>
2023-02-15 14:20:58 -0800

    Finished High Speed Chase

--- PlayerControlDeLaRosa/Assets/Course Library/Scripts/PlayerController.cs
+++ PlayerControlDeLaRosa/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 20;
+    public float turnSpeed;
+    public float horizontalInput;
 
     // Update is called once per frame
     void Update()
     {
-        //Move the Vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
